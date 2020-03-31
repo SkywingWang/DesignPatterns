eda24d587c07c2b82f5aa45b300ea8b76b8ec16e
Skywing <wangty9153@126.com>
2020-03-31 17:50:27 +0800

    工厂模式

---
+++ src/main/factoryPattern/AudiFactory.java
@@ -0,0 +1,9 @@
+package main.factoryPattern;
+
+/**
+ * Created by skywingking
+ * on 2020/3/31
+ */
+public abstract class AudiFactory {
+    public abstract <T extends AudiCar> T createAudiCar(Class<T> clz);
+}
